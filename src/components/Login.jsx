0c026776d52666e7fbe008d6f98fd5cbe18de10f
Yassine Azh <azhar.yassine2015@gmail.com>
2025-07-04 23:44:10 +0100

    using ref strat and formData browser object

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState("");
-  // const [enteredPassword, setEnteredPassword] = useState("");
-
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
-
-  // const handleEmailChange = (event) => {
-  //   setEnteredEmail(event.target.value);
-  // };
-
-  const handleInputChange = (identifier, event) => {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: event.target.value,
-    }));
-  };
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("User email :", enteredValues);
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    console.log(enteredEmail, enteredPassword);
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -31,24 +19,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => handleInputChange("email", event)}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) => handleInputChange("password", event)}
-            value={enteredValues.password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
