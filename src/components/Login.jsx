f9b1dfec8f775d55c732347fcfdba8b278fdc1f5
Yassine Azh <azhar.yassine2015@gmail.com>
2025-07-05 17:04:46 +0100

    different types of form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
   const email = useRef();
   const password = useRef();
 
@@ -10,7 +11,14 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -19,7 +27,11 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email} />
+          <input id="email" type="" name="email" ref={email} />
+          <div className="control-error">
+            {" "}
+            {emailIsInvalid && <p> Please enter a valid email</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
